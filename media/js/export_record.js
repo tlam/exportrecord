52af1370b29c8f9a808941344405e9934572bedf
tlam <tlam@lnx-tlam.(none)>
2010-02-13 17:10:42 -0500

    Displaying selected columns

--- media/js/export_record.js
+++ media/js/export_record.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     $("tr:odd").css("background-color", "#EDF3FE");
 
-    $("a#advanced-search").click(function(e) {
+    $("a#display-columns").click(function(e) {
         $("div#select-columns").toggle("slow");
+        $("th").toggle();
+        $("td").toggle();
         e.preventDefault();
+    });
+
+    $(".column-display").click(function(e) {
+        var name = "." + $(this).attr("name");
+        $("th" + name).toggle();
+        $("td" + name).toggle();
     });
 });
