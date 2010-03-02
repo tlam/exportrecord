b590330f744e2828b83d5985d7949f318780e820
tlam <tlam@lnx-tlam.(none)>
2010-03-01 22:18:59 -0500

    Adding login required middleware

--- settings.py
+++ settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'export.middleware.RequireLoginMiddleware',
 )
 
 ROOT_URLCONF = 'exportrecord.urls'
--- urls.py
+++ urls.py
@@ -1,18 +1,9 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^exportrecord/', include('exportrecord.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^', include('export.urls', namespace='export'), name='records'),
     url(r'^clients/', include('clients.urls', namespace='clients'), name='clients'),
